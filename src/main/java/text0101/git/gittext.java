223b3720121e4271427b81a8dc13fca39bf8e3ee
Raphael <598462841@qq.com>
2022-11-27 15:25:29 +0800

    ms-2 cm

--- src/main/java/text0101/git/gittext.java
+++ src/main/java/text0101/git/gittext.java
@@ -6,6 +6,6 @@ public class gittext {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
-
+        System.out.println("ms-text");
     }
 }
